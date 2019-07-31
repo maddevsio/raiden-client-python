7383a86ccfdc0f1a9bb5d629bc21e89e194ad9c0
Aleksandr Sobolev <thesobolev@gmail.com>
2019-07-31 17:49:13 +0600

    #3 Token registry endpoint

--- tests/endpoints/test_token_registry.py
+++ tests/endpoints/test_token_registry.py
@@ -1,9 +1,9 @@
-from raidenpy.endpoints.deploy_tokens import DeployTokenRequst
+from raidenpy.endpoints.token_register import TokenRegistryRequest
 
 
 def test_deploy_tokens():
     token_address = "0x123"
-    request = DeployTokenRequst(token_address=token_address)
+    request = TokenRegistryRequest(token_address=token_address)
     assert request.endpoint == f"/tokens/{token_address}"
     assert request.method == "put"
     assert not request.payload()
