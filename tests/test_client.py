ee1c9c2bb402c0b0051a966b297bf0074eee377e
Aleksandr Sobolev <thesobolev@gmail.com>
2019-08-01 09:21:25 +0600

    Remove client test, going to re-write it

--- tests/test_client.py
+++
@@ -1,6 +0,0 @@
-from raiden_client import Client
-
-
-def test_client_version():
-    client = Client(endpoint="http://api-url", version="v2")
-    assert client.handler.endpoint == "http://api-url"
