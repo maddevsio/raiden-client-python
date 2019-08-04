56f4369b2bf6fc055c269d6bdfbae38401d16802
Aleksandr Sobolev <thesobolev@gmail.com>
2019-08-04 12:49:08 +0600

    Added dummy cli test

--- tests/interfaces/test_cli.py
+++ tests/interfaces/test_cli.py
@@ -1,5 +1,10 @@
-from raiden_client import Client
+import sys
+from raiden_client.interfaces.cli import main
 
 
-def test_client_address():
-    pass
+def test_cli() -> None:
+    """Simple test which just try to build CLI parser"""
+    # When we run pytest tests/ , tests/ arg passed to CLI,
+    # So we should remove this argument:
+    sys.argv.pop()
+    assert main() is None
